b8b4707b88c3ee841132728c554e3836c90cb48a
Daniel Ciotti <daniel.ciotti@eadbox.com>
2020-01-17 12:12:04 -0300

    Enum changes

--- lib/search_object/plugin/enum.rb
+++ lib/search_object/plugin/enum.rb
@@ -34,7 +34,7 @@ module SearchObject
 
           return handle_invalid_value(object: object, option: option, enums: enums, scope: scope, value: value) unless enums.include? value.to_s
 
-          object.send("apply_#{Helper.underscore(option)}_with_#{Helper.underscore(value)}", scope)
+          object.send("apply_#{Helper.underscore(option)}", scope)
         end
 
         def handle_invalid_value(object:, option:, enums:, scope:, value:)
